031100a7a4cef9476e7b85f04fca84f5aacbae00
braze31 <braze31@mail.ru>
2016-07-22 15:38:16 +0300

    My Django

--- website/settings.py
+++ website/settings.py
@@ -123,5 +123,6 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
+# or, eg,
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
